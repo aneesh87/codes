c6bb2936d9e7cf87f3034f362177fd63975b7f67
Aneesh Gupta <agupta27@ncsu.edu>
2020-12-11 20:05:21 -0800

    Create letter-combinations-of-a-phone-number.cpp
    letter-combinations-of-a-phone-number
    
    Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
    A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.

---
+++ recursion/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,35 @@
+// Recursion:
+// can be done bottom up too
+class Solution {
+private:
+    // mapping to the alphabets
+    vector<string>cmap = {"", "", "abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    vector<string> combos(string &digits, int idx) {
+        vector<string> result;
+        vector<string> new_result;
+        
+        if (idx == digits.length()) {
+            new_result.push_back("");
+            return new_result;
+        }
+
+        result = combos(digits, idx+1);
+        
+        int digit =digits.at(idx) - '0';
+        for(int i =0; i< cmap[digit].length(); i++) {
+            for (int j=0; j< result.size(); j++) {
+                new_result.push_back(cmap[digit].at(i) + result[j]);
+            }
+        }
+        return new_result;
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        if (digits.length() == 0) {
+            return result;
+        }
+        result = combos(digits, 0);
+        return result;
+    }
+};
